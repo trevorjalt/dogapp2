73dac827eddbd4cd7569add8e9d28df997e159ce
Trevor Alt <trevorjalt@gmail.com>
2020-08-17 19:58:21 -0400

    added image number selection feature

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import $ from 'jquery';
 
-// start by getting the dog pic api
+function main(limit) {
+  let url = `https://dog.ceo/api/breeds/image/random/${limit}`
 
-
-// set up page to take image and display them
-
-// make sure it takes between 1 and 50 random pics
-// feature of a form that indicates the number the user wants
-
-function main () {
-  let url = 'https://dog.ceo/api/breeds/image/random/3';
-  console.log('DOM is loaded');
 
   fetch(url)
     .then(response => response.json())
@@ -19,41 +11,18 @@ function main () {
     .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-//   const startMsg = $('<p>Dog Webpack is working!</p>');
-//   $('#root').append(startMsg);
-
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    main();
+    const limit = $('#js-max-results').val();
+    main(limit);
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
-  watchForm();
-});
-
-
 function displayResults(responseJson) {
   console.log(responseJson.message);
-    let images = responseJson.message.map(url => `<img src="${url}" class="results-img">` );
-    $('.hidden').html(images);
+  let images = responseJson.message.map(url => `<img src="${url}" class="results-img">` );
+  $('.hidden').html(images);
 }
-  // return ` 
-  //     //replace the existing image with the new one
-  //     $('.results-img').replaceWith(
-  //       `<img src="${responseJson.message}" class="results-img">`
-  //     )
-  //     //display the results section
-  //     $('.results').removeClass('hidden');
-  //   }`
 
-
-
-
-
-
-  
-
-
+$(watchForm);
