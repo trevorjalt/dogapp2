adc2a8393024fa85dee956984ef936990f9cd5d7
Trevor Alt <trevorjalt@gmail.com>
2020-08-17 21:08:14 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 
 function main(limit) {
-  let url = `https://dog.ceo/api/breeds/image/random/${limit}`
+  let url = `https://dog.ceo/api/breed/${limit}/images/random`;
 
 
   fetch(url)
@@ -14,15 +14,20 @@ function main(limit) {
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    const limit = $('#js-max-results').val();
+    const limit = $('#js-breed-results').val();
     main(limit);
   });
 }
 
 function displayResults(responseJson) {
+  let image = `<img src="" class ="results-img">`
   console.log(responseJson.message);
-  let images = responseJson.message.map(url => `<img src="${url}" class="results-img">` );
-  $('.hidden').html(images);
+  if (responseJson.message === 'Breed not found (master breed does not exist)') {
+    image = `<p>Sorry, we cannot find a picture of your requested breed. Sad days.`;
+  } else {
+    image = `<img src="${responseJson.message}" class="results-img">`;
+  }
+  $('.hidden').html(image);
 }
 
 $(watchForm);
